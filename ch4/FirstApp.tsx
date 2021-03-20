1a82cbddf525cbb810c54edbecb63f7e305e43d6
JojiKoike <joji.koike@gmail.com>
2021-03-20 11:08:46 +0900

    :+1: mod text

--- ch4/FirstApp.tsx
+++ ch4/FirstApp.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function MyComponent() {
   return (
     <View>
-      <Text>土屋太鳳のおっぱいもみたい</Text>
+      <Text>MyFirst App</Text>
     </View>
   );
 }
