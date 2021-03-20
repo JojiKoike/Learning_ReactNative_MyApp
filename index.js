fd8521feae55908eb113da66171671343f096302
JojiKoike <joji.koike@gmail.com>
2021-03-20 11:07:44 +0900

    :+1: MyFirst Component

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+//import App from './App';
+import FirstApp from './ch4/FirstApp';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => FirstApp);
