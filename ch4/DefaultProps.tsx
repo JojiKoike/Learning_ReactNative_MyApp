51c43d45bc4943b9440ac55224e1a99fd9eaceff
JojiKoike <joji.koike@gmail.com>
2021-03-21 10:40:21 +0900

    :+1: Default Props

---
+++ ch4/DefaultProps.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  label: {
+    marginTop: 100,
+    padding: 8,
+    fontSize: 24,
+  },
+});
+
+interface Props {
+  backgroundColor: string;
+  color: string;
+  label: string;
+}
+
+const CustomLabel = (props: Props) => {
+  const { backgroundColor, color, label } = props;
+  return <Text style={[styles.label, { backgroundColor, color }]}>{label}</Text>;
+};
+
+CustomLabel.defaultProps = {
+  backgroundColor: '#008080',
+  color: 'white',
+  label: 'Hello',
+};
+
+export default CustomLabel;
