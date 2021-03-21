7f5b2a9bde677f916d728987205967bab42ac0ee
JojiKoike <joji.koike@gmail.com>
2021-03-21 15:32:00 +0900

    :+1: Ref

---
+++ ch4/Ref.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Text, TextInput, View, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    width: 160,
+    height: 32,
+    borderWidth: 1,
+    padding: 4,
+  },
+});
+
+const App = () => {
+  const ref = React.useRef<TextInput>(null);
+
+  return (
+    <View style={styles.container}>
+      <TextInput ref={ref} style={styles.input} />
+      <Text
+        onPress={() => {
+          ref?.current?.focus();
+        }}
+      >
+        focus
+      </Text>
+    </View>
+  );
+};
+
+export default App;
