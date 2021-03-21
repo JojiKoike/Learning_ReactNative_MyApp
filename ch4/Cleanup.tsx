2b4638f7c0151259d30a0607af8fdc63ae0f240d
JojiKoike <joji.koike@gmail.com>
2021-03-21 13:26:10 +0900

    :+1: Cleanup

---
+++ ch4/Cleanup.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+const A = () => {
+  React.useEffect(() => {
+    console.log('A effect');
+    return () => {
+      console.log('A cleanup');
+    };
+  });
+  return <Text>A</Text>;
+};
+
+const B = () => {
+  React.useEffect(() => {
+    console.log('B effect');
+    return () => {
+      console.log('B cleanup');
+    };
+  });
+  return <Text>B</Text>;
+};
+
+const App = () => {
+  const [needsToShowA, setNeedsToShowA] = React.useState(true);
+
+  return (
+    <View style={styles.container}>
+      <Text onPress={() => setNeedsToShowA(!needsToShowA)}>表示切り替え</Text>
+      {needsToShowA ? <A /> : <B />}
+    </View>
+  );
+};
+
+export default App;
