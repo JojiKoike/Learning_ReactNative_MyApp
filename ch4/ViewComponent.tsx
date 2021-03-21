f3b3751a7f4eb0eab4e7b49daa0fcd6a1491bda3
JojiKoike <joji.koike@gmail.com>
2021-03-21 17:11:56 +0900

    :+1: View Component

---
+++ ch4/ViewComponent.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, Text, ViewStyle } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>1行目</Text>
+      <Text>2行目</Text>
+    </View>
+  );
+};
+
+const Fragment = () => {
+  return (
+    <>
+      <Text>1行目</Text>
+      <Text>2行目</Text>
+    </>
+  );
+};
+
+interface Props {
+  style?: ViewStyle;
+}
+
+const MyComponent = (props: Props) => {
+  return (
+    <View style={[props.style]}>
+      <Text>スタイル設定可能なコンポーネント</Text>
+    </View>
+  );
+};
+
+export default Fragment;
