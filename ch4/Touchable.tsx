b924d0e5b4195c58c7d8aaade05b742f37496bbe
JojiKoike <joji.koike@gmail.com>
2021-03-26 18:56:12 +0900

    :+1: Touchable Component

---
+++ ch4/Touchable.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableHighlight, TouchableOpacity, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          console.log('TouchableOpacity');
+        }}
+      >
+        <View style={styles.button}>
+          <Text>TouchableOpacity</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableHighlight
+        disabled={true}
+        onPress={() => {
+          console.log('TouchableHighlight');
+        }}
+      >
+        <View style={styles.button}>
+          <Text>TouchableHighlight</Text>
+        </View>
+      </TouchableHighlight>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: 'red',
+    width: 300,
+    height: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 8,
+  },
+});
+
+export default App;
