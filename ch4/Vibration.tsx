a7674e3c0f42792be654f5f453eb0d8a6919eb05
JojiKoike <joji.koike@gmail.com>
2021-03-27 16:26:19 +0900

    :+1: Vibrate

---
+++ ch4/Vibration.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, Vibration, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          Vibration.vibrate(1000);
+        }}
+      >
+        <Text>Vibrate</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
