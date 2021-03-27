f16d02dacb16cefd76c58ce28b2eccdb6d91c72d
JojiKoike <joji.koike@gmail.com>
2021-03-28 08:21:44 +0900

    :+1: Animations

---
+++ ch4/Animations.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Animated, View, StyleSheet, Easing } from 'react-native';
+
+const App = () => {
+  const [width] = React.useState(new Animated.Value(0));
+  const [color] = React.useState(new Animated.Value(0));
+
+  React.useState(() => {
+    Animated.parallel([
+      Animated.timing(color, {
+        toValue: 100,
+        duration: 2500,
+      }),
+      Animated.sequence([
+        Animated.spring(width, {
+          toValue: 256,
+          friction: 4,
+        }),
+        Animated.timing(width, {
+          toValue: 0,
+          duration: 1500,
+          easing: Easing.bounce,
+        }),
+      ]),
+    ]).start(() => {
+      setTimeout(() => {
+        width.setValue(0);
+        color.setValue(0);
+      }, 100);
+    });
+  }, []);
+
+  const backgroundColor = color.interpolate({
+    inputRange: [0, 100],
+    outputRange: ['rgb(0,128,128)', 'rgb(128,0,128)'],
+  });
+
+  return (
+    <View style={styles.container}>
+      <Animated.View style={[styles.block, { width, backgroundColor }]} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  block: {
+    height: 32,
+  },
+});
+
+export default App;
