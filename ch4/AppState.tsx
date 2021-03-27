96774a0ef46872b0845223cea187f9e635f8eee1
JojiKoike <joji.koike@gmail.com>
2021-03-27 12:21:49 +0900

    :+1: AppState

---
+++ ch4/AppState.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Alert, AppState, StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  const [state, setState] = React.useState<string>(AppState.currentState);
+
+  const setAppState = (newState: string) => {
+    setState(newState);
+    if (newState === 'active') {
+      Alert.alert('active');
+    }
+  };
+
+  React.useEffect(() => {
+    AppState.addEventListener('change', setAppState);
+    return () => {
+      AppState.removeEventListener('change', setAppState);
+    };
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>{state}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
