6d5ffc087db75d2d4a7a0d699e280bc8e7744b1e
JojiKoike <joji.koike@gmail.com>
2021-03-28 07:58:06 +0900

    :+1: AnimatedProgressView

---
+++ ch4/AnimatedProgressView.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Animated, ViewStyle } from 'react-native';
+import { ProgressView } from '@react-native-community/progress-view';
+
+interface Props {
+  progress: number;
+  color: string;
+  style: ViewStyle;
+}
+
+const AnimatedProgressView = Animated.createAnimatedComponent(ProgressView);
+
+const Progress = (props: Props) => {
+  const [progress] = React.useState(new Animated.Value(0));
+
+  React.useEffect(() => {
+    Animated.spring(progress, {
+      toValue: props.progress,
+      friction: 4,
+    }).start();
+  }, [props.progress]);
+
+  return <AnimatedProgressView progress={progress} style={props.style} progressTintColor={props.color} />;
+};
+
+Progress.defaultProps = {
+  color: '#0a7ffb',
+};
